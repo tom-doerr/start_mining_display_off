f9f2c28daf39b56125e28f6ca630f2aca1e243fb
Tom Dörr <tom.doerr@tum.de>
2021-04-16 02:04:37 +0200

    Update file

--- main.sh
+++ main.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
+export CUDA_VISIBLE_DEVICES=0
+
 command="$@"
 
+display_off() {
+    [[ ! $(xset q | awk 'END{print $3}') == 'On' ]]
+}
 
-xset q
+while true
+do
+    if display_off
+    then
+        if [[ $pid_miner == '' ]]
+        then
+            echo 'Starting ethminer...'
+            ethminer -U -P stratum1+tcp://0xaa3e2211dd1c7e6a55d46e5af8a873e9967d1926@eth.2miners.com:2020 &
+            pid_miner=$!
+        else
+            printf 'Already running  '
+            date
+        fi
+    else
+        printf 'Display on, waiting for display to turn off...    '
+        date
+        if [ -v pid_miner ]
+        then
+            printf "Killing the miner with PID $pid_miner."
+            kill $pid_miner
+            unset pid_miner
+        fi
+    fi
+    sleep 1
+done
